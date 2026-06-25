e54db328b06898fbf0867def2e224508963c328f
Adarsh Sharma <144541909+Adarsh18198@users.noreply.github.com>
2026-06-25 20:39:30 +0530

    Update 05_talent_index.sql

--- sql/05_talent_index.sql
+++ sql/05_talent_index.sql
@@ -1,1 +1,10 @@
+/* ============================================================
+   Formula 1 Driver Talent Evaluation Framework
+   File: 05_talent_index.sql
 
+   Purpose:
+   Using the previously calculated metrics in driver_performance_summary
+   view to estimate driver talent, using a talent_index metric.
+
+   Author: Adarsh Sharma
+============================================================ */
